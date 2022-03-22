7c904f967530438cf2fef3d9dc73549a8772768e
Old Tang <307717971@qq.com>
2022-03-22 09:47:07 +0800

    Add files via upload

---
+++ CP2102N_GPIO/mt7628_boot.py
@@ -0,0 +1,42 @@
+import sys
+import usb.core
+# find USB devices
+#dev = usb.core.find(find_all=True)
+# loop through devices, printing vendor and product ids in decimal and hex
+#for cfg in dev:
+#    print(cfg)
+#    print('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
+#    print('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
+
+
+import time
+import usb.core
+import usb.util
+
+PID = 0xea60
+VID = 0x10c4
+
+dev = usb.core.find(idVendor=VID, idProduct=PID)
+if not dev:
+        print("CP2104 was not found!")
+        exit(1)
+print("Yeeha! Found CP2104")
+
+reqType = 0x41
+bReq = 0xFF
+wVal = 0x37E1
+
+#while True:
+
+print("set CS1 to Hihg")
+wIndex = 0xf1ff
+dev.ctrl_transfer(reqType, bReq, wVal, wIndex, [])
+time.sleep(1)
+print("set CS1 to High & RST mt7628")
+wIndex = 0xf5ff
+dev.ctrl_transfer(reqType, bReq, wVal, wIndex, [])
+time.sleep(1)
+print("set CS1 to High & release RST")
+wIndex = 0xf1ff
+dev.ctrl_transfer(reqType, bReq, wVal, wIndex, [])
+
